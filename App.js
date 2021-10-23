deab2aa45996091da983dfd2b8afd352d24f588c
nickjiang2378 <41227221+nickjiang2378@users.noreply.github.com>
2021-10-23 16:35:35 -0700

    Revert "inital"
    This reverts commit b3ad4ff7cbbafe6dfc90933940c7fef370e0f845.

--- App.js
+++ App.js
@@ -1,42 +1,21 @@
-import React, { useRef, useState, useEffect } from "react";
-import { AppState, Text, View, SafeAreaView} from "react-native";
-import { ProgressBar, Button, Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { styles } from "./AppStyles" 
-import firebase from "firebase"
-import JoinRoom from "./screens/JoinRoom"
-import StudyRoom from "./screens/StudyRoom"
-
-const firebaseConfig = require("./keys.json")
-if (firebase.apps.length == 0) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-const RootStack = createNativeStackNavigator()
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <RootStack.Navigator>
-          <RootStack.Screen 
-            name="JoinRoom" 
-            options={{title: "Join a Room"}}
-            component={JoinRoom} 
-          />
-          <RootStack.Screen 
-            name="StudyRoom" 
-            options={{title: "Your Room"}}
-            component={StudyRoom} 
-          />
-        </RootStack.Navigator>
-      </NavigationContainer>
-    </PaperProvider> 
-
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
